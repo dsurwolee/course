c8e810693a06d0bcd0798c14fad742286578cff2
Daniel Lee <34401571+dsurwolee@users.noreply.github.com>
2022-12-22 21:58:45 -0500

    basic authentication

--- src/index.js
+++ src/index.js
@@ -2,7 +2,57 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
+
+import { initializeApp } from "firebase/app";
+import { GoogleAuthProvider, getAuth, signInWithPopup, signInWithEmailAndPassword, createUserWithEmailAndPassword, sendPasswordResetEmail, signOut } from "firebase/auth";
+import { getFirestore, query, getDocs, collection, where, addDoc} from "firebase/firestore";
+
+// Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAMsiGIVP6-NGgqWQHYQMyIZLIPZOTFZSo",
+  authDomain: "course-64784.firebaseapp.com",
+  projectId: "course-64784",
+  storageBucket: "course-64784.appspot.com",
+  messagingSenderId: "803028178061",
+  appId: "1:803028178061:web:e05781401487b4eeddfec4",
+  measurementId: "G-0NCMBCLQK4"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+const googleProvider = new GoogleAuthProvider();
+
 const el = document.getElementById('root');
 const root = ReactDOM.createRoot(el);
 
-root.render(<App />)
+const signInWithGoogle = async () => {
+    try {
+      const res = await signInWithPopup(auth, googleProvider);
+      const user = res.user;
+      const q = query(collection(db, "users"), where("uid", "==", user.uid));
+      const docs = await getDocs(q);
+      if (docs.docs.length === 0) {
+        await addDoc(collection(db, "users"), {
+          uid: user.uid,
+          name: user.displayName,
+          authProvider: "google",
+          email: user.email,
+        });
+      }
+    } catch (err) {
+      console.error(err);
+      alert(err.message);
+    }
+  };
+
+root.render(<button onClick={signInWithGoogle}>Login with Google</button>)
+// root.render(<App />)
